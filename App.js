16ce789ba66033272308fb7529bc14b25360c6ac
Yevgeniy <ulchenkov2001@gmail.com>
2024-04-06 13:53:10 +0200

    [Upd] updated native-safe-area context

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {Avatar, Button, Card, MD3LightTheme, PaperProvider, Text} from 'react-native-paper';
+import {View, StyleSheet} from "react-native";
+import ListCard from "./components/ListCard";
+const theme = {
+    ...MD3LightTheme,
+    roundness: 2,
+    colors: {
+        ...MD3LightTheme.colors,
+        primary: '#663399',
+        secondary: '#f1c40f',
+        tertiary: '#a1b2c3',
+    },
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Kyrya privet z expo react native</Text>
-      <StatusBar style="auto" />
-    </View>
+    const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+      return (
+          <PaperProvider theme={theme}>
+             <ListCard></ListCard>
+          </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
