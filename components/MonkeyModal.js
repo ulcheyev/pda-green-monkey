62d7e122617f0c128179ba46a41b270fb66b6df0
Yevgeniy <ulchenkov2001@gmail.com>
2024-04-11 00:16:26 +0200

    [Upd] Added: Modal window, API from modal exposed, Create list modal

---
+++ components/MonkeyModal.js
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { useState } from "react";
+import { Button, IconButton, Text, useTheme } from "react-native-paper";
+import {
+  KeyboardAvoidingView,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+const MonkeyModal = (props) => {
+  const [text, setText] = useState("");
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    modalContainer: {
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      ...(props.modalContainerStyle ?? {}),
+    },
+
+    modalContent: {
+      backgroundColor: "#fff",
+      padding: 20,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+    },
+
+    backdrop: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+
+    header: {
+      alignItems: "center",
+      marginBottom: 10,
+
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+
+    title: {
+      ...(props.titleStyle ?? {
+        fontSize: 20,
+        textAlign: "center",
+        color: theme.colors.tertiary,
+        flex: 2,
+      }),
+    },
+    closeIconStyle: {
+      tintColor: theme.colors.tertiary,
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "flex-end",
+    },
+    leftSideHeaderItems: {
+      flex: 1,
+    },
+  });
+
+  const closeModal = () => {
+    props.hideModal();
+  };
+
+  return (
+    <View>
+      <Modal
+        transparent={true}
+        animationType={"fade"}
+        visible={props.visible}
+        onRequestClose={closeModal}
+        statusBarTranslucent={true}
+      >
+        <View style={styles.container}>
+          <TouchableWithoutFeedback onPress={closeModal}>
+            <View style={styles.backdrop} />
+          </TouchableWithoutFeedback>
+          <KeyboardAvoidingView
+            behavior={"padding"}
+            style={styles.modalContainer}
+          >
+            <View style={props.modalContentStyle ?? styles.modalContent}>
+              <View style={props.headerStyle ?? styles.header}>
+                <View style={styles.leftSideHeaderItems}>
+                  {props.leftSideHeaderItems}
+                </View>
+                <Text style={styles.title}>{props.title}</Text>
+                <View style={styles.closeIconStyle}>
+                  <IconButton
+                    iconColor={theme.colors.tertiary}
+                    size={25}
+                    icon={"close"}
+                    onPress={closeModal}
+                  />
+                </View>
+              </View>
+              {props.children}
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default MonkeyModal;
--- screens/ShoppingLists.js
+++ screens/ShoppingLists.js
@@ -1,15 +1,25 @@
-import { FAB, Modal, Portal, Text, useTheme } from "react-native-paper";
+import {
+  Button,
+  FAB,
+  HelperText,
+  Text,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
 import ListCard from "../components/ListCard";
 import { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import DataManager from "../services/DataManager";
-import ModalCreateList from "../components/ModalCreateList";
+import MonkeyModal from "../components/MonkeyModal";
+import * as React from "react";
+import { text } from "ionicons/icons";
 
 const ShoppingLists = () => {
+  const MAX_LIST_NAME_LENGTH = 30;
   const [visible, setVisible] = useState(false);
   const dataManager = new DataManager();
   const [lists, setLists] = useState(dataManager.getTestData()); //todo make this dynamic
-
+  const [creationListName, setCreationListName] = React.useState("");
   const theme = useTheme();
   const styles = StyleSheet.create({
     listsContainer: {
@@ -26,20 +36,83 @@ const ShoppingLists = () => {
       backgroundColor: theme.colors.primary,
       borderRadius: 18,
     },
+    button: {
+      marginBottom: 15,
+      backgroundColor: "#85AF9D",
+    },
+    buttonTitleStyle: {
+      color: theme.colors.tertiary,
+    },
+    modalContainer: {
+      borderTopRightRadius: 15,
+      borderTopLeftRadius: 15,
+    },
+    modalContentStyle: {
+      padding: 15,
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: theme.colors.primary,
+      borderTopRightRadius: 15,
+      borderTopLeftRadius: 15,
+    },
+    listNameInput: {
+      borderTopRightRadius: 10,
+      borderTopLeftRadius: 10,
+      backgroundColor: theme.colors.tertiary,
+    },
+
+    templateContainer: {
+      flex: 1,
+      flexDirection: "row",
+      width: "100%",
+      gap: 20,
+      alignItems: "center",
+      marginBottom: 25,
+    },
+
+    templateLabel: {
+      color: theme.colors.tertiary,
+      fontSize: 17,
+    },
+
+    templateButton: {
+      flexGrow: 1,
+      backgroundColor: theme.colors.tertiary,
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      borderBottomLeftRadius: 10,
+      borderBottomRightRadius: 10,
+      marginTop: 10,
+    },
+
+    templateButtonTitleStyle: {
+      color: "#565555",
+    },
   });
 
   const fabOnPress = () => {
+    setVisible(true);
+  };
+
+  const hideModal = () => {
+    setVisible(false);
+    setCreationListName("");
+  };
+
+  const createList = (list) => {
     const newList = {
       id: lists.length + 1,
-      name: "My New Super Super Super List",
+      name: creationListName,
       isTemplate: false,
       progress: 0,
       items: [],
     };
-    setVisible(true);
+    setLists([...lists, newList]);
+    setVisible(false);
+    setCreationListName("");
   };
 
-  const createListHideModal = () => setVisible(false);
+  const onChangeCreationListName = (text) => setCreationListName(text);
 
   return (
     <View style={styles.listsContainer}>
@@ -64,11 +137,39 @@ const ShoppingLists = () => {
         style={styles.fab}
         onPress={fabOnPress}
       />
-      <ModalCreateList
+      <MonkeyModal
         visible={visible}
-        hideModal={createListHideModal}
-        setLists={setLists}
-      />
+        hideModal={hideModal}
+        modalContentStyle={styles.modalContentStyle}
+        modalContainerStyle={styles.modalContainer}
+        title={"Create a list"}
+      >
+        <View>
+          <TextInput
+            label="List name"
+            value={creationListName}
+            onChangeText={onChangeCreationListName}
+            style={styles.listNameInput}
+          />
+        </View>
+        <View style={styles.templateContainer}>
+          <Text style={styles.templateLabel}>Template</Text>
+          <Button
+            style={styles.templateButton}
+            onPress={createList}
+            labelStyle={styles.templateButtonTitleStyle}
+          >
+            Choose template...
+          </Button>
+        </View>
+        <Button
+          style={styles.button}
+          onPress={createList}
+          labelStyle={styles.buttonTitleStyle}
+        >
+          Create
+        </Button>
+      </MonkeyModal>
     </View>
   );
 };
