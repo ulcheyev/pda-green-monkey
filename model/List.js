764f53c8169fdd9163cf638acb35c88b7fe7eea0
heraskyr <heraskyr@gmail.com>
2024-04-29 15:37:01 +0200

    Added progress to list

--- model/List.js
+++ model/List.js
@@ -3,13 +3,30 @@ class List {
     this.name = name;
     this.isTemplate = false;
     this.shops = [];
+    this.progress = updateProgress();
   }
 
   addShop(shop) {
     this.shops.push(shop);
+    this.progress = this.updateProgress();
   }
 
   removeShop(shop) {
     this.shops = this.shops.filter((sh) => sh.id != shop.id);
+    this.progress = this.updateProgress();
+  }
+
+  updateProgress() {
+    let numItems = 0;
+    let checkedItems = 0;
+    this.shops.map((shop) =>
+      shop.items.map((item) => {
+        numItems++;
+        if (item.checkedItems) {
+          checkedItems++;
+        }
+      }),
+    );
+    return (checkedItems / numItems) * 100;
   }
 }
